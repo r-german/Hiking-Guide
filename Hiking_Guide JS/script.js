391422500d0aba0a0ee3375b6669a630f97cda11
r-german <111256133+r-german@users.noreply.github.com>
2023-10-19 00:19:19 +0300

    Update script.js

--- Hiking_Guide JS/script.js
+++ Hiking_Guide JS/script.js
@@ -1,9 +1,6 @@
 // gsap
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-gsap.registerPlugin(ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
-    smooth: 3,
     effects: true,
-    smoothTouch: 0.1,
-    ignoreMobileResize: true,
+    normalizeScroll: true,
 });
